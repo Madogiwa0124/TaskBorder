07f8ae6b22c3188e9d975d61276d1b9f83683707
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2016-07-17 13:14:43 +0900

    初期commit

---
+++ script.js
@@ -0,0 +1,94 @@
+var tasks = []
+var taskId = 0
+$(function () {
+  // 期限の初期値は現在日付を設定
+  $('#limit').val(getformatDate('yyyy-mm-dd'))
+
+  // 登録ボタンの取得
+  var inputBtn = document.getElementById('inputBtn')
+
+  inputBtn.addEventListener('click', function () {
+    // 画面情報の取得
+    var form = {}
+    form.title = escapeHtml($('#title').val())
+    form.priority = $('#priority').attr('value')
+    form.text  = escapeHtml($('#text').val())
+    form.limit = $('#limit').val()
+    // 新規タスクの生成
+    var task = new Task(form)
+    task.createTask()
+    tasks.push(task)
+    console.log(form);
+  })
+})
+
+// タスクの削除
+// $function()内に記載するとhtmlから呼び出せないため、外出し
+function taskDelete (id) {
+  // 削除対象のタスクの取得
+  var deleteTask = tasks[id]
+  // タスクの削除
+  deleteTask.deleteTask()
+  tasks.splice(this.id, this.id)
+}
+
+function taskStatusChange (id, order) {
+  // 変更対象のタスクの取得
+  var statusChangeTask = tasks[id]
+
+  // 状態変更前のタスクの削除
+  // 状態変更後では、対象を見つけられるErrorとなるため先に削除
+  statusChangeTask.deleteTask()
+  // タスクの状態変更
+  statusChangeTask.statusChangeTask(order)
+  // 状態変更後のタスクの生成
+  statusChangeTask.createTask()
+}
+
+// HTMLエスケープ処理
+function escapeHtml(str) {
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&#39;');
+    return str;
+}
+
+// 日付の整形メソッド
+function getformatDate (format) {
+  var date = new Date()
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  // 指定した桁数に0パディングする関数を格納
+  var toTargetDigits = function (num, digits) {
+    num += ''
+    while (num.length < digits) {
+      num = '0' + num
+    }
+    return num
+  }
+
+  // format変換
+  switch (format) {
+    case 'yyyymmdd':
+      var yyyy = toTargetDigits(year, 4)
+      var mm = toTargetDigits(month, 2)
+      var dd = toTargetDigits(day, 2)
+      return yyyy+mm+dd
+    case 'yyyy-mm-dd':
+      var yyyy = toTargetDigits(year, 4)
+      var mm = toTargetDigits(month, 2)
+      var dd = toTargetDigits(day, 2)
+      return yyyy+'-'+mm+'-'+dd
+    case 'yyyy/mm/dd':
+      var yyyy = toTargetDigits(year, 4)
+      var mm = toTargetDigits(month, 2)
+      var dd = toTargetDigits(day, 2)
+      return yyyy+'/'+mm+'/'+dd
+    default:
+      break
+  }
+}
---
+++ task.js
@@ -0,0 +1,88 @@
+class Task {
+  // コンストラクター
+  // 入力したタスクの情報を取得
+  constructor (form) {
+    this.id = taskId
+    this.title = form.title
+    this.priority = form.priority
+    this.text = form.text
+    this.limit = form.limit
+    // TODO：登録時に状態を選択出来るようにする。
+    this.status = 'todo'
+    taskId++
+  }
+
+  // タスク作成メソッド
+  createTask () {
+    // 反映先の取得
+    var insertPlace = document.getElementById(this.status)
+    // タスクのHTMLオブジェクトの生成
+    var insertElement = document.createElement('div')
+    // クラスの追加
+    insertElement.className = 'task'
+    insertElement.id = this.id
+    // HTML文字列を生成
+    insertElement.innerHTML =
+      '<p class="task-title ' + this.getTitleColor() + '">' + this.title + '</p>'
+      + '<p class="task-text">' + this.text + '</p>'
+      + '<p class="task-info">'
+      + '期限：<input type="date" value="' + this.limit + '">'
+      + '<span class="task-btn">'
+      + '<span class="btn-group">'
+      + '<button class="btn btn-xs btn-success downBtn" onClick="taskStatusChange(' + this.id + ',\'down\')">◀</button>'
+      + '<button class="btn btn-xs btn-success upBtn"   onClick="taskStatusChange(' + this.id + ',\'up\')">▶</button>'
+      + '</span>'
+      + '<button class="btn btn-xs btn-danger deleteBtn" onClick="taskDelete(' + this.id + ')">削除</button>'
+      + '</span>'
+      + '</p>'
+    console.log(insertElement.innerHTML)
+    // 反映先に作成したタスクのHTMLオブジェクトを反映
+    insertPlace.appendChild(insertElement)
+  }
+
+  // タスクタイトルの背景色取得メソッド
+  getTitleColor() {
+    // 追加するクラスと重要度の関連を保持した連想配列
+    var color = { high: 'bg-danger', normal: 'bg-success', low: 'bg-info', default: 'bg-default' }
+    console.log(this.status)
+    // 引数と連想配列の引当を行い、結果をreturn
+    switch (this.priority) {
+      case 'high':
+        return color.high
+      case 'normal':
+        return color.normal
+      case 'low':
+        return color.low
+      default:
+        return color.default
+    }
+  }
+
+  // タスク削除メソッド
+  deleteTask () {
+    // 削除元の取得
+    var deletePlace = document.getElementById(this.status)
+    // 削除するタスクのHTMLオブジェクトの取得
+    var deleteElement = document.getElementById(this.id)
+    // 画面からHTMLオブジェクトを削除
+    deletePlace.removeChild(deleteElement)
+  }
+
+  // タスクの状態変更メソッド
+  statusChangeTask(order) {
+
+    // タスクの状態を"up"かつ進捗が"done"でなければタスクの進捗をUP
+    if (order === 'up' && this.status !== 'done') {
+      this.status = this.status === 'todo' ? 'doing' : 'done'
+    }
+    // タスクの状態を"down"かつ進捗が"todo"でなければタスクの進捗をDOWN
+    else if (order === 'down' && this.status !== 'todo') {
+      this.status = this.status === 'done' ? 'doing' : 'todo'
+    }
+    // それ以外の場合は、Errorをコンソールへ出力
+    else {
+      console.log('error[method:taskStatusChange order:'+order+' taskStatus:'+this.status+']')
+    }
+  }
+
+}
