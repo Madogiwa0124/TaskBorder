4356357aa042936324c9ba2197337b0e01766972
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2016-08-15 17:55:13 +0900

    必須チェック機能追加及びレイアウト微修正

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ $(function () {
   // 期限の初期値は現在日付を設定
   $('#limit').val(getformatDate('yyyy-mm-dd'))
   // タイトルにフォーカスを設定
-  $('#title').focus() 
+  $('#title').focus()
   // 登録ボタン押下時タスクを登録
   $('#inputBtn').click(function () {
+    // 必須入力チェック
+    if (!requiredCheck()) return
     var form = {}
     form.title = escapeHtml($('#title').val())
     form.priority = $('#priority').attr('value')
-    form.text  = escapeHtml($('#text').val())
+    form.text = escapeHtml($('#text').val())
     form.limit = $('#limit').val()
     // 新規タスクの生成
     var task = new Task(form)
@@ -25,14 +27,37 @@ $(function () {
     // 期限は現在日付へリセット
     $('#limit').val(getformatDate('yyyy-mm-dd'))
     // タイトルへフォーカスを設定
-    $('#title').focus()    
+    $('#title').focus()
   })
 })
-  
 
+// 必須入力チェック
+function requiredCheck() {
+  var errFields = []
+  // 必須項目の入力チェック
+  $('.required').each(function () {
+    // 値がなかったらErrorMsgを作成
+    if ($(this).val() == '') {
+      errFields.push($(this).attr('id'))
+      console.log(errFields)
+      var errMsg = errFields[errFields.length - 1] + ' is required.'
+      $('#alert').append('<li>' + errMsg + '</li>')
+    }
+  })
+  // エラーが有った場合、alertエリアを表示
+  if (errFields.length > 0) {
+    $('#alert').removeClass('hidden');
+    $('#' + errFields[0]).focus()
+    return false
+  } else {
+    $('#alert li').remove()
+    $('#alert').addClass('hidden')
+    return true
+  }
+}
 // タスクの削除
 // $function()内に記載するとhtmlから呼び出せないため、外出し
-function taskDelete (id) {
+function taskDelete(id) {
   // 削除対象のタスクの取得
   var deleteTask = tasks[id]
   // タスクの削除
@@ -40,7 +65,7 @@ function taskDelete (id) {
   tasks.splice(this.id, this.id)
 }
 
-function taskStatusChange (id, order) {
+function taskStatusChange(id, order) {
   // 変更対象のタスクの取得
   var statusChangeTask = tasks[id]
 
@@ -55,17 +80,17 @@ function taskStatusChange (id, order) {
 
 // HTMLエスケープ処理
 function escapeHtml(str) {
-    str = str.replace(/&/g, '&amp;');
-    str = str.replace(/</g, '&lt;');
-    str = str.replace(/>/g, '&gt;');
-    str = str.replace(/"/g, '&quot;');
-    str = str.replace(/'/g, '&#39;');
-    str = str.replace(/[\n\r]/g,'<br />')
-    return str;
+  str = str.replace(/&/g, '&amp;');
+  str = str.replace(/</g, '&lt;');
+  str = str.replace(/>/g, '&gt;');
+  str = str.replace(/"/g, '&quot;');
+  str = str.replace(/'/g, '&#39;');
+  str = str.replace(/[\n\r]/g, '<br />')
+  return str;
 }
 
 // 日付の整形メソッド
-function getformatDate (format) {
+function getformatDate(format) {
   var date = new Date()
   var year = date.getFullYear()
   var month = date.getMonth() + 1
