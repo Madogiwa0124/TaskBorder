373bcbd94d272777e06768e71aad9365f4710f0c
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2016-09-18 12:13:16 +0900

    共通methodを隔離、期限の初期値を翌時間の0分に設定するよう変更

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var tasks = []
 var taskId = 0
 $(function () {
   // 期限の初期値は現在日付を設定
-  $('#limit').val(getformatDate('yyyy-MM-ddThh:mm'))
+  $('#limit').val(getformatDate('yyyy-MM-ddThh:mm','new'))
   // タイトルにフォーカスを設定
   $('#title').focus()
 })
@@ -29,7 +29,7 @@ $('#resetBtn').click(function () {
   $('#title').val('')
   $('#text').val('')
   // 期限は現在日付へリセット
-  $('#limit').val(getformatDate('yyyy-MM-ddThh:mm'))
+  $('#limit').val(getformatDate('yyyy-MM-ddThh:mm','new'))
   // タイトルへフォーカスを設定
   $('#title').focus()
 })
@@ -81,54 +81,8 @@ function taskStatusChange(id, order) {
   statusChangeTask.createTask()
 }
 
-// HTMLエスケープ処理
-function escapeHtml(str) {
-  str = str.replace(/&/g, '&amp;');
-  str = str.replace(/</g, '&lt;');
-  str = str.replace(/>/g, '&gt;');
-  str = str.replace(/"/g, '&quot;');
-  str = str.replace(/'/g, '&#39;');
-  str = str.replace(/[\n\r]/g, '<br />')
-  return str;
-}
-
 // * をチェックボックスに置換
 function replaceCheckBox(str) {
   var ret = str.replace(/\*/g, '<input type="checkbox">')
   return ret  
-}
-
-// 日付の整形メソッド
-function getformatDate(format) {
-  var date = new Date()
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-  var hour = date.getHours()
-  var min = date.getMinutes()
-
-  // 指定した桁数に0パディングする関数を格納
-  var toTargetDigits = function (num, digits) {
-    num += '' // 文字列型へ変更
-    while (num.length < digits) {
-      num = '0' + num
-    }
-    return num
-  }
-
-  // 日付の0パディング
-  var yyyy = toTargetDigits(year, 4)
-  var MM = toTargetDigits(month, 2)
-  var dd = toTargetDigits(day, 2)
-  var hh = toTargetDigits(hour, 2)
-  var mm = toTargetDigits(min, 2)
-
-  // format変換
-  switch (format) {
-    case 'yyyyMMdd':   return yyyy + MM + dd
-    case 'yyyy-MM-dd': return yyyy + '-' + MM + '-' + dd
-    case 'yyyy/MM/dd': return yyyy + '/' + MM + '/' + dd
-    case 'yyyy-MM-ddThh:mm': return yyyy + '-' + MM + '-' + dd + 'T' + hh + ':' + mm
-    default: break  // なにもしない。
-  }
 }
